7983e0add8db4f4ffa6cf2aa2713909f8a209726
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2023-10-16 19:37:59 -0300

    Basic oil properties by Standing ok

---
+++ sample/pvt.py
@@ -0,0 +1,530 @@
+import math
+
+class PVT:
+
+    def __init__(self):
+        self._api = None
+        self._do = None
+        self._dw = None
+        self._dg = None
+        self._rho_air = 1.2 # kg/m3
+        self._rhow = 1000. # kg/m3
+
+        self._t = None
+        self._p = None
+        self._t_std = 20.
+        self._p_std = 1.01325 # 1 atm
+
+        self._rs = None
+        self._p_bubble = None
+        self._gor = None
+
+        self._bo = None
+        self._bg = None
+        self._bw = None
+        
+        self._bo_bubble = None
+        self._co_bubble = None
+        
+        self._t_pc = None
+        self._p_pc = None
+        self._t_pr = None
+        self._p_pr = None
+        self._y_co2 = 0.
+        self._y_h2s = 0.
+        self._y_n2 = 0.
+        self._z = None
+
+        self._units = {
+            'api':'oAPI',
+            'do':'-',
+            'dg':'-',
+            'dw':'-',
+            'rhoo':'kg/m3',
+            'rhog':'kg/m3',
+            'rhow':'kg/m3',
+            't':'oC',
+            'p':'bar',
+            'gor':'std m3/std m3',
+            'rs':'std m3/std m3',
+            'p_bubble':'bar',
+            'bo':'-',
+            'bg':'-',
+            'bw':'-',
+            'bo_bubble':'-',
+            'co_bubble':'1/bar',
+            't_pc':'oC',
+            'p_pc':'bar',
+            't_pr':'-',
+            'p_pr':'-',
+            'y_co2':'-',
+            'y_h2s':'-',
+            'y_n2':'-',
+            'z':'-',
+            }
+        pass
+
+    def get_variables_list(self):
+        out = dict()
+        for k,v in self._units.items():
+            out[k] = v
+        return out
+    def print_variables_list(self):
+        for k,v in self._units.items():
+            print(f'{k} [{v}]')
+
+    def get_unit(self, var):
+        if var.lower() in self._units.keys():
+            return self._units[var.lower()]
+
+    def _check_value(self,value, min_value, max_value):
+        if value < min_value or value > max_value:
+            raise NameError(f'Invalid value ({value}). Valid values: [{min_value},{max_value}].')
+
+    def set_api(self, api):
+        self._check_value(api,1.,100.)
+        self._api = api
+        self._do = 141.5 / (131.5 + self._api)
+    def set_do(self, do):
+        self._check_value(do,0.01,10.)
+        self._do = do
+        self._api = 141.5 / self._do - 131.5
+    def set_rho(self, rhoo):
+        self._check_value(rhoo,10.,10000.)
+        self.set_do(rhoo / self._rhow)
+    def set_dg(self, dg):
+        self._check_value(dg,0.1,10.)
+        self._dg = dg
+    def set_rhog(self, rhog):
+        self._check_value(rhog / self._rho_air,0.1,10.)
+        self._dg = rhog / self._rho_air
+    def set_dw(self, dw):
+        self._check_value(dw,0.01,10.)
+        self._dw = dw
+    def set_rhow(self, rhow):
+        self._check_value(rhow,10.,10000.)
+        self._dg = rhow / self._rhow
+    def set_gor(self, gor):
+        self._check_value(gor,0.,100000.)
+        self._gor = gor
+    def set_t(self, t):
+        self._check_value(t,-273.15,1.e6)
+        self._t = t
+    def set_p(self, p):
+        self._check_value(p,0.01,1.e6)
+        self._p = p
+    def set_rs(self, rs):
+        self._check_value(rs,0.,100000.)
+        self._rs = rs
+    def set_p_bubble(self, p):
+        self._check_value(p,0.01,1.e6)
+        self._p_bubble = p
+    def set_bo(self, bo):
+        self._check_value(bo,0.01,10.)
+        self._bo = bo
+    def set_bg(self, bg):
+        self._check_value(bg,1e-20,10.)
+        self._bg = bg
+    def set_bw(self, bw):
+        self._check_value(bw,0.01,10.)
+        self._bw = bw
+    def set_bo_bubble(self, bo):
+        self._check_value(bo,0.01,10.)
+        self._bo_bubble = bo
+    def set_co_bubble(self, co):
+        self._check_value(co,1e-20,1e6)
+        self._co_bubble = co
+    def set_t_pc(self, t):
+        self._check_value(t,-273.15,1.e6)
+        self._t_pc = t
+    def set_p_pc(self, p):
+        self._check_value(p,0.01,1.e6)
+        self._p_pc = p
+    def set_t_pr(self, t):
+        self._check_value(t,1e-20,1e6)
+        self._t_pr = t
+    def set_p_pr(self, p):
+        self._check_value(p,1e-20,1e6)
+        self._p_pr = p
+    def set_y_co2(self, y):
+        self._check_value(y,0,1.)
+        self._y_co2 = y
+    def set_y_h2s(self, y):
+        self._check_value(y,0,1.)
+        self._y_h2s = y
+    def set_y_n2(self, y):
+        self._check_value(y,0,1.)
+        self._y_n2 = y
+    def set_y_z(self, z):
+        self._check_value(z,1e-20,1e6)
+        self._z = z
+
+    def reset_API(self):
+        self._api = None
+        self._do = None
+    def reset_do(self):
+        self.reset_API()
+    def reset_rhoo(self):
+        self.reset_API()
+    def reset_dg(self):
+        self._dg = None
+    def reset_rhog(self):
+        self._dg = None
+    def reset_dw(self):
+        self._dw = None
+    def reset_rhow(self):
+        self._dw = None
+    def reset_t(self):
+        self._t = None
+    def reset_p(self):
+        self._p = None
+    def reset_tp(self):
+        self.reset_t()
+        self.reset_p()
+    def reset_rs(self):
+        self._rs = None
+    def reset_gor(self):
+        self._gor = None
+    def reset_p_bubble(self):
+        self._p_bubble = None
+    def reset_bo(self):
+        self._bo = None
+    def reset_bg(self):
+        self._bg = None
+    def reset_bw(self):
+        self._bw = None
+    def reset_bo_bubble(self):
+        self._bo_bubble = None
+    def reset_co_bubble(self):
+        self._co_bubble = None
+    def reset_t_pc(self):
+        self._t_pc = None
+    def reset_p_pc(self):
+        self._p_pc = None
+    def reset_t_pr(self):
+        self._t_pr = None
+    def reset_p_pr(self):
+        self._p_pr = None
+    def reset_y_co2(self):
+        self._y_co2 = None
+    def reset_y_h2s(self):
+        self._y_h2s = None
+    def reset_y_n2(self):
+        self._y_n2 = None
+    def reset_z(self):
+        self._z = None
+
+    def get_API(self):
+        return self._api
+    def get_do(self):
+        return self._do
+    def get_rhoo(self):
+        return self._do * self._rhow
+    def get_dg(self):
+        return self._dg
+    def get_rhog(self):
+        return self._dg * self._rho_air
+    def get_dw(self):
+        return self._dw
+    def get_rhow(self):
+        return self._dw * self._rhow
+    def get_t(self):
+        return self._t
+    def get_p(self):
+        return self._p
+    def get_rs(self):
+        return self._rs
+    def get_gor(self):
+        return self._gor
+    def get_p_bubble(self):
+        return self._p_bubble
+    def get_bo(self):
+        return self._bo
+    def get_bg(self):
+        return self._bg
+    def get_bw(self):
+        return self._bw
+    def get_bo_bubble(self):
+        return self._bo_bubble
+    def get_co_bubble(self):
+        return self._co_bubble
+    def get_t_pc(self):
+        return self._t_pc
+    def get_p_pc(self):
+        return self._p_pc
+    def get_t_pr(self):
+        return self._t_pr
+    def get_p_pr(self):
+        return self._p_pr
+    def get_y_co2(self):
+        return self._y_co2
+    def get_y_h2s(self):
+        return self._y_h2s
+    def get_y_n2(self):
+        return self._y_n2
+    def get_z(self):
+        return self._z
+
+    def _check_api(self):
+        if self._api is None:
+            raise NameError('Oil density API not set.')
+        return True
+    def _check_do(self):
+        return self._check_api
+    def _check_dg(self):
+        if self._dg is None:
+            raise NameError('Gas density not set.')
+        return True
+    def _check_dw(self):
+        if self._dw is None:
+            raise NameError('Water density not set.')
+        return True
+    def _check_t(self):
+        if self._t is None:
+            raise NameError('In place temperature not set.')
+        return True
+    def _check_p(self):
+        if self._p is None:
+            raise NameError('In place pressure not set.')
+        return True
+    def _check_rs(self):
+        if self._rs is None:
+            raise NameError('Oil solubility ratio not set/estimated.')
+        return True
+    def _check_gor(self):
+        if self._gor is None:
+            raise NameError('Gas oil ratio not set/estimated.')
+        return True
+    def _check_p_bubble(self):
+        if self._p_bubble is None:
+            raise NameError('Oil bubble pressure not set/estimated.')
+        return True
+    def _check_bo(self):
+        if self._bo is None:
+            raise NameError('Oil formation volume factor not set/estimated.')
+        return True
+    def _check_bg(self):
+        if self._bg is None:
+            raise NameError('Gas formation volume factor not set/estimated.')
+        return True
+    def _check_bw(self):
+        if self._bw is None:
+            raise NameError('Water formation volume factor not set/estimated.')
+        return True
+    def _check_bo_bubble(self):
+        if self._bo_bubble is None:
+            raise NameError('Oil formation volume factor at the bubble point not set/estimated.')
+        return True
+    def _check_co_bubble(self):
+        if self._co_bubble is None:
+            raise NameError('Oil compressibility at the bubble point not set/estimated.')
+        return True
+    def _check_t_pc(self):
+        if self._t_pc is None:
+            raise NameError('Pseudo-critical temperature not set/estimated.')
+        return True
+    def _check_p_pc(self):
+        if self._p_pc is None:
+            raise NameError('Pseudo-critical pressure not set/estimated.')
+        return True
+    def _check_t_pr(self):
+        if self._t_pr is None:
+            raise NameError('Pseudo-reduced temperature not set/estimated.')
+        return True
+    def _check_p_pr(self):
+        if self._p_pr is None:
+            raise NameError('Pseudo-reduced pressure not set/estimated.')
+        return True
+    def _check_y_co2(self):
+        if self._y_co2 is None:
+            raise NameError('CO2 content in gas not set.')
+        return True
+    def _check_y_h2s(self):
+        if self._y_h2s is None:
+            raise NameError('H2S content in gas not set.')
+        return True
+    def _check_y_n2(self):
+        if self._y_n2 is None:
+            raise NameError('N2 content in gas not set.')
+        return True
+    def _check_z(self):
+        if self._z is None:
+            raise NameError('Gas Z factor not set/estimated.')
+        return True
+
+    def _check(self, variables):
+        for variable in variables:
+            match variable.lower():
+                case 'api':
+                    _ = self._check_api()
+                case 'do':
+                    _ = self._check_do()
+                case 'dg':
+                    _ = self._check_dg()
+                case 'dw':
+                    _ = self._check_dw()
+                case 't':
+                    _ = self._check_t()
+                case 'p':
+                    _ = self._check_p()
+                case 'rs':
+                    _ = self._check_rs()
+                case 'gor':
+                    _ = self._check_gor()
+                case 'p_bubble':
+                    _ = self._check_p_bubble()
+                case 'bo':
+                    _ = self._check_bo()
+                case 'bg':
+                    _ = self._check_bg()
+                case 'bw':
+                    _ = self._check_bw()
+                case 'bo_bubble':
+                    _ = self._check_bo_bubble()
+                case 'co_bubble':
+                    _ = self._check_co_bubble()
+                case 't_pc':
+                    _ = self._check_t_pc()
+                case 'p_pc':
+                    _ = self._check_p_pc()
+                case 't_pr':
+                    _ = self._check_t_pr()
+                case 'p_pr':
+                    _ = self._check_p_pr()
+                case 'y_co2':
+                    _ = self._check_y_co2()
+                case 'y_h2s':
+                    _ = self._check_y_h2s()
+                case 'y_n2':
+                    _ = self._check_y_n2()
+                case 'z':
+                    _ = self._check_z()
+                case '_':
+                    raise NameError('Unknown variable.')
+        return True
+
+    def calculate_rs_Standing(self):
+        self._check(['dg','p','api','t','gor'])
+        x = 0.0125 * self._api - 0.00091 * (1.8 * self._t + 32)
+        y = self._p * pow(10, x)
+        z = 0.1373 * self._dg * pow(y, 1.205)
+        self._rs = min(z, self._gor)
+        
+    def calculate_p_bubble_Standing(self):
+        self._check(['dg','gor','api','t'])
+        x = 0.0125 * self._api - 0.00091 * (1.8 * self._t + 32)
+        y = self._gor / self._dg / 0.1373
+        self._p_bubble = pow(y, 1/1.205) * pow(10, -x)
+
+    def calculate_p_bubble_Standing_Original(self):
+        self._check(['dg','gor','api','t'])
+        x = 0.0125 * self._api - 0.00091 * (1.8 * self._t + 32)
+        y = 5.615 * self._gor / self._dg
+        z = pow(y, 0.83) * pow(10, -x)
+        self._p_bubble = 1.305 * (z - 1.4)
+
+    def calculate_co_bubble_Standing(self, auto=False):
+        self._check(['t','dg','api'])
+        if auto:
+            if self._rs is None:
+                self.calculate_rs_Standing()
+            if self._p_bubble is None:
+                self.calculate_p_bubble_Standing()
+        else:
+            self._check(['rs','p_bubble'])
+        x = -1433. + 5 * 5.615 * self._gor
+        y = 17.2*(1.8 * self._t + 491.67)
+        z = -1180. * self._dg + 12.61 * self._api
+        w = 1E5 * self._p_bubble
+        self._co_bubble = (x + y + z) / w
+
+    def calculate_bo_bubble_Standing(self):
+        self._check(['dg','do','t','gor'])
+        x = 5.615 * self._gor * pow(self._dg / self._do, 0.5)
+        y = 1.25 * (1.8 * self._t + 32)
+        self._bo_bubble = 0.9759 +  12E-5 * pow(x + y, 1.2)
+
+    def calculate_bo_Standing(self, auto=False):
+        self._check(['p'])
+        if auto:
+            if self._p_bubble is None:
+                self.calculate_p_bubble_Standing()
+        else:
+            self._check(['p_bubble'])
+        if self._p > self._p_bubble:
+            if auto:
+                if self._bo_bubble is None:
+                    self.calculate_bo_bubble_Standing()
+                if self._co_bubble is None:
+                    self.calculate_co_bubble_Standing()
+            else:
+                self._check(['co_bubble','bo_bubble'])
+            self._bo = self._bo_bubble * (1 + self._co_bubble * (self._p_bubble - self._p))
+        else:
+            self._check(['dg','do','t'])
+            if auto:
+                if self._rs is None:
+                    self.calculate_rs_Standing()
+            else:
+                self._check(['rs'])
+            x = 5.615 * self._rs * pow(self._dg / self._do, 0.5)
+            y = 1.25 * (1.8 * self._t + 32)
+            self._bo = 0.9759 +  12E-5 * pow(x + y, 1.2)
+
+    def calculate_p_pc_Standing(self):
+        self._check(['dg','y_co2','y_h2s','y_n2'])
+        x = 706. - 51.7 * self._dg - 11.1 * self._dg * self._dg
+        y = 440. * self._y_co2 + 600. * self._y_h2s - 170. * self._y_n2
+        self._p_pc = (x + y) / 14.503773773375086
+
+    def calculate_t_pc_Standing(self, auto=False):
+        self._check(['dg','y_co2','y_h2s','y_n2'])
+        x = 187. + 330. * self._dg - 71.5 * self._dg * self._dg
+        y = - 80. * self._y_co2 + 130. * self._y_h2s - 250. * self._y_n2
+        self._t_pc = (x + y - 491.67) * 5./9.
+    
+    def calculate_p_pr(self, auto=False):
+        self._check(['p'])
+        if auto:
+            if self._p_pc is None:
+                self.calculate_p_pc_Standing()
+        else:
+            self._check(['p_pc'])
+        self._p_pr = self._p / self._p_pc
+
+    def calculate_t_pr(self, auto=False):
+        self._check(['t'])
+        if auto:
+            if self._t_pc is None:
+                self.calculate_t_pc_Standing()
+        else:
+            self._check(['t_pc'])
+        self._t_pr = self._t / self._t_pc
+            
+    def calculate_z_Standing(self, auto=False):
+        if auto:
+            if self._p_pr is None:
+                self.calculate_p_pr(True)
+            if self._t_pr is None:
+                self.calculate_t_pr(True)
+        else:
+            self._check(['p_pr','t_pr'])
+        # self._check_value(self._p_pr, 0, 13)
+        # self._check_value(self._t_pr, 1.2, 2.4)
+        p = self._p_pr
+        t = self._t_pr
+        a = 1.39 * pow(t - 0.92, 0.5) - 0.36 * t - 0.101
+        b = (0.62 - 0.23 * t) * p + ( 0.066/(t - 0.86) -0.037) * p * p + 0.32 / pow(10, 9*(t - 1)) * pow(p,6)
+        c = 0.132 - 0.32 * math.log10(t)
+        d = pow(10, 0.3106 - 0.49 * t + 0.1824 *t * t)
+        self._z = a + (1 - a) / math.exp(b) + c * pow(p, d)
+
+    def calculate_bg(self, auto=False):
+        self._check(['p','t'])
+        if auto:
+            if self._z is None:
+                self.calculate_z_Standing()
+        else:
+            self._check(['z'])
+        self._bg = self._z * self._p_std / self._p * (self._t + 273.15) / (self._t_std + 273.15)
+
---
+++ tests/pvt_tests.py
@@ -0,0 +1,183 @@
+import os
+from context import pvt
+import matplotlib.pyplot as plt  
+
+path = os.path.abspath(os.path.dirname(__file__))
+def save_plot(plt, name):
+    plt.savefig(path+'/plots/'+name+'.png')
+    # plt.show()
+
+def rs_test():
+    pvt1 = pvt.PVT()
+    pvt1.set_api(25.)
+    pvt1.set_dg(0.8)
+    pvt1.set_t(80.)
+
+    p_init = 1
+    p_end = 500
+    steps = 200
+    p = [p_init + i/(steps-1)*(p_end - p_init) for i in range(steps)]
+    rs = {40:[], 80:[], 120:[]}
+
+    fig = plt.figure() 
+    for rsi in rs.keys():
+        pvt1.set_gor(rsi)
+        for pi in p:
+            pvt1.set_p(pi)
+            pvt1.calculate_rs_Standing()
+            rs[rsi].append(pvt1.get_rs())
+        plt.plot(p, rs[rsi])  
+
+    ax = plt.gca() 
+    ax.legend(rs.keys()) 
+    plt.grid()
+    plt.xlabel('p [bar]')  
+    plt.ylabel('rs [m3/m3]')  
+    plt.title('Rs Using Standing Correlation')  
+    save_plot(plt,'rs')
+
+def pb_test():
+    pvt1 = pvt.PVT()
+    pvt1.set_api(25.)
+    pvt1.set_dg(0.8)
+    pvt1.set_t(80.)
+
+    gor_init = 1
+    gor_end = 200
+    steps = 200
+    gor = [gor_init + i/(steps-1)*(gor_end - gor_init) for i in range(steps)]
+    pb = {'Original':[], 'Modified':[]}
+    for gori in gor:
+        pvt1.set_gor(gori)
+        pvt1.calculate_p_bubble_Standing_Original()
+        pb['Original'].append(pvt1.get_p_bubble())
+        pvt1.calculate_p_bubble_Standing()
+        pb['Modified'].append(pvt1.get_p_bubble())
+    fig = plt.figure() 
+    plt.plot(gor, pb['Original'])  
+    plt.plot(gor, pb['Modified'])  
+
+    ax = plt.gca() 
+    ax.legend(pb.keys()) 
+    plt.grid()
+    plt.xlabel('GOR [m3/m3]')  
+    plt.ylabel('P bubble [bar]')  
+    plt.title('P Bubble Using Standing Correlation')  
+    save_plot(plt,'p_bubble')
+
+def pb_test2():
+    pvt1 = pvt.PVT()
+    pvt1.set_api(30.)
+    pvt1.set_gor(200.)
+    pvt1.set_dg(0.7)
+
+    pvt2 = pvt.PVT()
+    pvt2.set_api(15.)
+    pvt2.set_gor(20.)
+    pvt2.set_dg(0.8)
+
+    t_init = 1
+    t_end = 120
+    steps = 200
+    t = [t_init + i/(steps-1)*(t_end - t_init) for i in range(steps)]
+    pb = {'light':[], 'heavy':[]}
+    for ti in t:
+        pvt1.set_t(ti)
+        pvt1.calculate_p_bubble_Standing()
+        pb['light'].append(pvt1.get_p_bubble())
+        
+        pvt2.set_t(ti)
+        pvt2.calculate_p_bubble_Standing()
+        pb['heavy'].append(pvt2.get_p_bubble())
+    fig = plt.figure() 
+    plt.plot(t, pb['light'])  
+    plt.plot(t, pb['heavy'])  
+
+    ax = plt.gca() 
+    ax.legend(pb.keys()) 
+    plt.grid()
+    plt.xlabel('GOR [m3/m3]')  
+    plt.ylabel('P bubble [bar]')  
+    plt.title('P Bubble Using Standing Correlation')  
+    save_plot(plt,'p_bubble2')
+
+def bo_test():
+    pvt1 = pvt.PVT()
+    pvt1.set_api(30.)
+    pvt1.set_gor(200.)
+    pvt1.set_dg(0.7)
+    pvt1.set_t(80.)
+
+    pvt2 = pvt.PVT()
+    pvt2.set_api(15.)
+    pvt2.set_gor(20.)
+    pvt2.set_dg(0.8)
+    pvt2.set_t(80.)
+
+    p_init = 100
+    p_end = 400
+    steps = 500
+    p = [p_init + i/(steps-1)*(p_end - p_init) for i in range(steps)]
+    bo = {'light':[], 'heavy':[]}
+    for pi in p:
+        pvt1.set_p(pi)
+        pvt1.calculate_rs_Standing()
+        pvt1.calculate_bo_Standing(auto=True)
+        bo['light'].append(pvt1.get_bo())
+        
+        pvt2.set_p(pi)
+        pvt2.calculate_rs_Standing()
+        pvt2.calculate_bo_Standing(auto=True)
+        bo['heavy'].append(pvt2.get_bo())
+    fig = plt.figure() 
+    plt.plot(p, bo['light'])  
+    plt.plot(p, bo['heavy'])  
+
+    ax = plt.gca() 
+    ax.legend(bo.keys()) 
+    plt.grid()
+    plt.xlabel('p [bar]')  
+    plt.ylabel('Bo [m3/m3]')  
+    plt.title('Bo Using Standing Correlation')  
+    save_plot(plt,'bo')    
+
+def dg_test():
+    pvt1 = pvt.PVT()
+    pvt1.set_dg(0.6)
+    pvt1.set_t(80.)
+
+    pvt2 = pvt.PVT()
+    pvt2.set_dg(0.9)
+    pvt2.set_t(80.)
+
+    p_init = 1
+    p_end = 500
+    steps = 500
+    p = [p_init + i/(steps-1)*(p_end - p_init) for i in range(steps)]
+    dg = {'dg=0.6':[], 'dg=0.9':[]}
+    for pi in p:
+        pvt1.set_p(pi)
+        pvt1.calculate_z_Standing(auto=True)
+        dg['dg=0.6'].append(pvt1.get_z())
+        
+        pvt2.set_p(pi)
+        pvt2.calculate_z_Standing(auto=True)
+        dg['dg=0.9'].append(pvt2.get_z())
+    fig = plt.figure() 
+    plt.plot(p, dg['dg=0.6'])  
+    plt.plot(p, dg['dg=0.9'])  
+
+    ax = plt.gca() 
+    ax.legend(dg.keys()) 
+    plt.grid()
+    plt.xlabel('p [bar]')  
+    plt.ylabel('z [-]')  
+    plt.title('Z Using Standing Correlation')  
+    save_plot(plt,'z')    
+
+if __name__ == "__main__":
+    # rs_test()
+    # pb_test()
+    # pb_test2()
+    # bo_test()
+    dg_test()
