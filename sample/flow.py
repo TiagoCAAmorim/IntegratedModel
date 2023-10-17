102eac7d819b174ecdf524c1e640557d33949adb
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2023-10-17 16:19:53 -0300

    Single flow element out pressure calculation ok

--- sample/flow.py
+++ sample/flow.py
@@ -5,6 +5,8 @@ import common
 class FlowElement:
 
     def __init__(self):
+        self._eps = 1E-6
+        self._max_iter = 100
         self._g = 9.81 # m/s^2
 
         self._z_in = 0
@@ -59,12 +61,117 @@ class FlowElement:
             're':['Reynolds number', '-'],
             'hl':['Head loss', 'm'],
             })
+        
+    def set_z_in(self,z):
+        self._z_in = z
+    def set_z_out(self,z):
+        self._z_out = z
+    def set_p_in(self,p):
+        self._p_in = p
+    def set_p_out(self,p):
+        self._p_out = p    
+    def set_t_in(self,t):
+        self._t_in = t
+    def set_t_out(self,t):
+        self._t_out = t
+    def set_q_in(self,q):
+        self._q_in = q
+    def set_q_out(self,q):
+        self._q_out = q
+    def set_v_in(self,v):
+        self._v_in = v
+    def set_v_out(self,v):
+        self._v_out = v
+    def set_d(self,d):
+        self._d = d
+    def set_e(self,e):
+        self._e = e
+    def set_h(self,h):
+        self._h = h
     
-    def calculate_velocity(self):
-        self._v = 4 * self._m_rate / (self.pvt.get_rhoo_in_place() * math.pi() * self._d ** 2)
+    def get_z_in(self):
+        return self._z_in
+    def get_z_out(self):
+        return self._z_out
+    def get_p(self):
+        return self._p
+    def get_p_in(self):
+        return self._p_in
+    def get_p_out(self):
+        return self._p_out
+    def get_t(self):
+        return self._t
+    def get_t_out(self):
+        return self._t_in
+    def get_t_out(self):
+        return self._t_out
+    def get_m_rate(self):
+        return self._m_rate
+    def get_q(self):
+        return self._q
+    def get_q_in(self):
+        return self._q_in
+    def get_q_out(self):
+        return self._q_out
+    def get_v(self):
+        return self._v
+    def get_v_in(self):
+        return self._v_in
+    def get_v_out(self):
+        return self._v_out
+    def get_d(self):
+        return self._d
+    def get_e(self):
+        return self._e
+    def get_h(self):
+        return self._h
+    def get_f(self):
+        return self._f
+    def get_re(self):
+        return self._re
+    def get_hl(self):
+        return self._hl
+
+    def calculate_p(self):
+        self._p = (self._p_in + self._p_out) / 2.
+    def calculate_t(self):
+        self._t = (self._t_in + self._t_out) / 2.
+    def calculate_q(self):
+        self._q = (self._q_in + self._q_out) / 2.
+    def calculate_v(self):
+        self._v = (self._v_in + self._v_out) / 2.
     
+    def calculate_rhoo(self, p, t):
+        self.pvt.set_t(t)
+        self.pvt.set_p(p)
+        self.pvt.calculate_rs_Standing()
+        self.pvt.calculate_p_bubble_Standing()
+        self.pvt.calculate_co_bubble_Standing()
+        self.pvt.calculate_bo_bubble_Standing()
+        self.pvt.calculate_bo_Standing()
+
+    def calculate_m_rate(self):
+        self.calculate_rhoo(self._p_in, self._t_in)
+        self._m_rate = self.pvt.get_rhoo_in_place() * self._q_in / (24*60*60)
+    
+    def calculate_v_in(self):
+        self.calculate_rhoo(self._p_in, self._t_in)
+        self._v_in = 4 * self._m_rate / (self.pvt.get_rhoo_in_place() * math.pi * self._d ** 2)
+
+    def calculate_v_out(self):
+        self.calculate_rhoo(self._p_out, self._t_out)
+        self._v_out = 4 * self._m_rate / (self.pvt.get_rhoo_in_place() * math.pi * self._d ** 2)
+    
+    def calculate_uo(self, p, t):
+        self.pvt.set_t(t)
+        self.pvt.set_p(p)
+        self.pvt.calculate_rs_Standing()
+        self.pvt.calculate_uo_do_Standing()
+        self.pvt.calculate_uo_Standing()
+
     def calculate_Reynolds(self):
-        self._re = 4 * self._m_rate / (self.pvt.get_uo() * math.pi() * self._d)
+        self.calculate_uo(self._p, self._t)
+        self._re = 4 * self._m_rate / (self.pvt.get_uo()/1000 * math.pi * self._d)
 
     def calculate_f(self):
         if self._re < 2300:
@@ -75,7 +182,40 @@ class FlowElement:
     def calculate_head_loss(self):
         self._hl = self._f * self._h * self._v ** 2 / (self._d * 2 * self._g)
 
+    def calculate_p_out(self):
+        self.calculate_rhoo(self._p_out, self._t_out)
+        dz = self._z_out - self._z_in
+        self.calculate_Reynolds()
+        self.calculate_f()
+        self.calculate_v_in()
+        self.calculate_v_out()
+        self.calculate_v()
+        self.calculate_head_loss()
+        hl = self._hl
+        htm = 0. # not implemented yet
+        dv2 = (self._v_out ** 2 - self._v_in ** 2) / (2 * self._g)
+        self._p_out = self._p_in - 1E-5 * self.pvt.get_rhoo_in_place() * self._g * (dz + hl + htm + dv2)
 
+    def solve_out_flow(self):
+        self.calculate_m_rate()
+        self.set_p_out(self._p_in)
+        self.set_t_out(self._t_in)
+        self.calculate_p()
+        self.calculate_t()
+        p_old = self.get_p()
+        t_old = self.get_t()
+        i = 0
+        while i<self._max_iter:        
+            self.calculate_p_out()
+            # self.calculate_t_out() # not implemented yet
+            self.calculate_p()
+            self.calculate_t()
+            if abs(p_old - self._p) < self._eps and abs(t_old - self._t) < self._eps:
+                break
+            p_old = self.get_p()
+            t_old = self.get_t()
+            i += 1
+        pass
 
 class IPR:
 
--- tests/flow_tests.py
+++ tests/flow_tests.py
@@ -1,12 +1,68 @@
 import os
 from context import flow
-import matplotlib.pyplot as plt  
+# import matplotlib.pyplot as plt  
 
-path = os.path.abspath(os.path.dirname(__file__))
-def save_plot(plt, name):
-    plt.savefig(path+'/plots/'+name+'.png')
-    # plt.show()
+# path = os.path.abspath(os.path.dirname(__file__))
+# def save_plot(plt, name):
+#     plt.savefig(path+'/plots/'+name+'.png')
+#     # plt.show()
+
+def define_common_parameters():
+    trunk = flow.FlowElement()
+    trunk.pvt.set_api(15.)
+    trunk.pvt.set_gor(20.)
+    trunk.pvt.set_dg(0.6)
+    trunk.set_t_in(50.)
+    trunk.set_d(6 * 2.54/100.)
+    trunk.set_e(0.6 / 1000)
+    return trunk
+
+def define_trunk1():
+    trunk = define_common_parameters()    
+    trunk.set_p_in(340)
+    trunk.set_h(1600.)
+    trunk.set_z_in(-1600. + -1320.)
+    trunk.set_z_out(-1320.)
+    return trunk
+
+def define_trunk2():
+    trunk = define_common_parameters()    
+    trunk.set_h(500.)
+    trunk.set_z_in(-1320.)
+    trunk.set_z_out(-1320.)
+    return trunk
+
+def define_trunk3():
+    trunk = define_common_parameters()    
+    trunk.set_h(1320.)
+    trunk.set_z_in(-1320.)
+    trunk.set_z_out(0.)
+    return trunk
+
+def horizontal_test():
+    print('1000 m Horizontal trunk')
+    trunk = define_trunk1()
+    trunk.set_z_in(0.)
+    trunk.set_z_out(0.)
+    trunk.set_q_in(1000.)
+    trunk.solve_out_flow()
+    print(f'  Pressure in: {trunk.get_p_in()} bar.')
+    print(f'  Pressure out: {trunk.get_p_out()} bar.')
+
+def vertical_test():
+    print('1000 m Vertical trunk')
+    trunk = define_trunk1()
+    trunk.set_z_in(0.)
+    trunk.set_z_out(1000.)
+    trunk.set_q_in(1000.)
+    trunk.solve_out_flow()
+    print(f'  Pressure in: {trunk.get_p_in()} bar.')
+    print(f'  Pressure out: {trunk.get_p_out()} bar.')
+
+# def horizontal_test():
+    # trunk = flow.FlowElement()
 
 
 if __name__ == "__main__":
-    pass
+    # horizontal_test()
+    vertical_test()
