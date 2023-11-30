ee200157a628eebd31a825acab272d2c2a0ede86
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2023-11-30 01:09:51 -0300

    Added water pump power demand calculation

--- sample/integration.py
+++ sample/integration.py
@@ -63,7 +63,11 @@ class Integration:
 
         self.flow_inj.pvt = self.pvt.copy()
         self.flow_inj.pvt.set_wfr(1.)
+        self.flow_inj.set_q_std(self.reservoir.get_qwi())
+        self.flow_inj.set_t_out(self._reservoir_t)
         self.flow_inj.update_pvt()
+
+        self.water_pump.set_qw(self.reservoir.get_qwi())
 
     def advance_simulation(self, dt):
         self.flow_prod.set_dt(dt)
@@ -77,10 +81,27 @@ class Integration:
         pwf_flow = self.flow_prod.get_p_in()[0]
         self._log(f' t = {t:0.2f} days, Pwf_res = {self._last_pwf:0.2f} bar, Qo = {qo:0.2f} m3/d, Qw = {qw:0.2f} m3/d, , Pwf_flow = {pwf_flow:0.2f} bar, Phead = {phead:0.2f} bar')
 
+    def solve_water_injection(self):
+        pwf_inj = self.reservoir.get_pr_cell(0,0)[-1]
+        self.flow_inj.set_p_out(pwf_inj)
+        self.flow_inj.solve_in_flow()
+        phead = self.flow_inj.get_p_in()[0]
+        power_pump = self.water_pump.get_power_from_delta_p(phead - 1.)
+        self._log(f' Pwf_injw = {pwf_inj:0.2f}, Phead_injw = {phead:0.2f} bar, Power = {power_pump:0.2f} MW')
+
+    def solve_gas_balance(self):
+        pass
+
+    def solve_emissions(self):
+        pass
+
     def run_simulation(self, dt):
         self.initialize()
         t = 0.
         while t < self.reservoir.get_t_end():
             dti = min(dt, self.reservoir.get_t_end() - t)
             self.advance_simulation(dti)
+            self.solve_water_injection()
+            self.solve_gas_balance()
+            self.solve_emissions()
             t += dti
